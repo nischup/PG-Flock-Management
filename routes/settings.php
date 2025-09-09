7688a19db651a20b810e7d3583e8e0a0acae5ec1
nischup <ahsabbir23@gmail.com>
2025-09-09 12:02:55 +0600

    ps receive supplier render based on shipment type

--- routes/settings.php
+++ routes/settings.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Ps\PsFirmReceiveController;
+use App\Http\Controllers\Ps\PsReceiveController;
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
-use App\Http\Controllers\UserRoleController;
 use App\Http\Controllers\UserRegisterController;
-use App\Http\Controllers\Ps\PsReceiveController;
-use App\Http\Controllers\Ps\PsFirmReceiveController;
+use App\Http\Controllers\UserRoleController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -26,22 +26,18 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('settings/Appearance');
     })->name('appearance');
 
-    
     Route::resource('ps-receive', PsReceiveController::class)->except(['show']);
     Route::resource('ps-firm-receive', PsFirmReceiveController::class)->except(['show']);
     Route::get('/ps-receive/{id}/data', [PsReceiveController::class, 'getData']);
     Route::post('ps-receive/storelab', [PsReceiveController::class, 'storelab']);
+    Route::get('/ps-receive/suppliers-by-shipment-type', [PsReceiveController::class, 'getSuppliersByShipmentType']);
     Route::resource('user-role', UserRoleController::class)->except(['show']);
-    
-    
+
     Route::get('/user-register', [UserRegisterController::class, 'index'])->middleware('permission:user.view')->name('users.index');
     Route::get('/user-register/create', [UserRegisterController::class, 'create'])->middleware('permission:user.create')->name('users.create');
     Route::post('/user-register', [UserRegisterController::class, 'store'])->middleware('permission:user.create')->name('users.store');
     Route::get('/user-register/{user}/edit', [UserRegisterController::class, 'edit'])->middleware('permission:user.edit')->name('users.edit');
     Route::put('/user-register/{user}', [UserRegisterController::class, 'update'])->middleware('permission:user.edit')->name('users.update');
     Route::delete('/user-register/{user}', [UserRegisterController::class, 'destroy'])->middleware('permission:user.delete')->name('users.destroy');
-
-
-    
 
 });
