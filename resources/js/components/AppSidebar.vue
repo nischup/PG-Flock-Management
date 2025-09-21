3efa51ab777ba785cb70dc61c10edf5c228c3448
kazimusabber <kazimusabber@gmail.com>
2025-09-21 15:23:34 +0600

    update sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -8,7 +8,7 @@ import { BabyChick } from '@/icons/BabyChick';
 import { BabyChickMultiple } from '@/icons/BabyChickMultiple';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/vue3';
-import { Building, Building2, Pipette, ClockArrowUp ,Egg, EggOff, LayoutGrid, Package, PencilRuler, Pill, Skull, Syringe, User, Users, FileText,Settings, CheckCircle, Layers  } from 'lucide-vue-next';
+import { Building, Building2, Pipette,TestTubes,EggFried, ClockArrowUp ,Egg, EggOff, LayoutGrid, Package, PencilRuler, Pill, Skull, Syringe, User, Users, FileText,Settings, CheckCircle, Layers  } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const page = usePage();
